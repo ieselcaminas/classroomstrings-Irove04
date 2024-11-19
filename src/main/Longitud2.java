a4260668996c29c14093d44c7eccbab43205ee6e
Irove04 <maresctor4@alu.edu.gva.es>
2024-11-19 13:50:46 +0100

    longitud 2

--- src/main/Longitud2.java
+++ src/main/Longitud2.java
@@ -1,22 +1,21 @@
 public class Longitud2 {
-    public static void main(String[] args) {
-        String cadena = "cama casa clase alumnos";
+    public static boolean longitud(String cadena, int num){
         String[] palabra = cadena.split(" ");
-        int num = 4;
-        boolean existe = false;
 
         for (int i = 0; i < palabra.length; i++) {
             if (palabra[i].length() == num){
-                existe = true;
-                break;
+                return true;
             }
 
         }
-        if (existe){
-            System.out.println("Hay alguna palabra con: " + num + " caracteres.");
-        }
-
-
+        return false;
     }
-}
+
+
+    public static void main(String[] args) {
+        String cadena = "cama casa clase alumnos";
+        System.out.println(longitud(cadena, 4));
+        }
+    }
+
 
